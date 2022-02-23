2f18e9cd5e7538a092e9afccef3de1b73400dbb3
tostream <benedictfwp@gmail.com>
2022-02-23 22:37:13 +0000

    Create RemoveElement.py

---
+++ leetcode/RemoveElement.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        length=0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[length] = nums[i]
+                length += 1
+        for i in range(length, len(nums)):
+            nums.pop()
+            
+        return length
