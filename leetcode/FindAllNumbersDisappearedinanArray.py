bac88ad7593985743448ac377885af8a006336d6
tostream <benedictfwp@gmail.com>
2022-02-26 23:17:21 +0000

    Update FindAllNumbersDisappearedinanArray.py

--- leetcode/FindAllNumbersDisappearedinanArray.py
+++ leetcode/FindAllNumbersDisappearedinanArray.py
@@ -2,3 +2,11 @@ class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         s = set(nums)
         return [i for i in range(1, len(nums)+1) if i not in s]
+
+#
+class Solution:
+    def findDisappearedNumbers(self, nums):        
+        for i in range(len(nums)):
+            while nums[nums[i]-1] != nums[i]: 
+                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+        return [i for i in range(1, len(nums)+1) if i != nums[i-1]]    
