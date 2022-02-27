7dbb7064ccb5fa75808b48768731f2584fac19ea
tostream <benedictfwp@gmail.com>
2022-02-27 22:33:21 +0000

    Create 145_BinaryTreePostorderTraversal.py

---
+++ leetcode/145_BinaryTreePostorderTraversal.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        def postorder(root,ans):
+            if not root:
+                return ans
+            postorder(root.left,ans)            
+            postorder(root.right,ans)
+            ans.append(root.val)
+            return ans
+        return postorder(root,[])
+        
+        ###
+        traversal, stack = [], [(root, False)]
+        while stack:
+            node, visited = stack.pop()
+            if node:
+                if visited:
+                    # add to result if visited
+                    traversal.append(node.val)
+                else:
+                    # post-order
+                    stack.append((node, True))
+                    stack.append((node.right, False))
+                    stack.append((node.left, False))
+
+        return traversal
