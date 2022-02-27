6406527e70064506d8b005fa99d7646a06a8b45d
tostream <benedictfwp@gmail.com>
2022-02-27 22:06:28 +0000

    Create 94_BinaryTreeInorderTraversal.py

---
+++ leetcode/94_BinaryTreeInorderTraversal.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        def dfs(node,ans):
+            if not node:
+                return None
+            dfs(node.left,ans)
+            ans.append(node.val)
+            dfs(node.right,ans)
+            return ans
+        return dfs(root,[])
+        
+        
+    # iteratively       
+    def inorderTraversal(self, root):
+        res, stack = [], []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return res
+            node = stack.pop()
+            res.append(node.val)
+            root = node.right
