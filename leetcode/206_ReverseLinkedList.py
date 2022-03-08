a46b2533df6d8b09ea999eb83373837d07799291
tostream <benedictfwp@gmail.com>
2022-03-08 15:51:18 +0000

    Update 206_ReverseLinkedList.py

--- leetcode/206_ReverseLinkedList.py
+++ leetcode/206_ReverseLinkedList.py
@@ -16,3 +16,19 @@ class Solution:
             ans.val = result[i]
         
         return itr_ans.next
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        temp_node = ListNode()
+        ans = None
+        while(head):
+            temp_node = head
+            head = head.next
+            temp_node.next = ans
+            ans = temp_node
+        return ans
